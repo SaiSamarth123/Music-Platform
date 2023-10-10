c1af1988a57d08fdf777efd5fe5fa04e240151d2
SaiSamarth123 <sai003sam@gmail.com>
2023-10-10 17:54:43 -0500

    work in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,52 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { Home, Login } from "./components";
 import React, { useEffect, useState } from "react";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  inMemoryPersistence,
+  signInWithPopup,
+} from "firebase/auth";
+import { app } from "./config/firebase.config";
 
 const App = () => {
+  const firebaseAuth = getAuth(app);
+  const navigate = useNavigate();
+
   const [auth, setAuth] = useState(
     false || window.localStorage.getItem("auth") === "true"
   );
+
+  useEffect(() => {
+    //setIsLoading(true);
+    firebaseAuth.onAuthStateChanged((userCred) => {
+      if (userCred) {
+        userCred.getIdToken().then((token) => {
+          console.log(token);
+          // window.localStorage.setItem("auth", "true");
+          // validateUser(token).then((data) => {
+          //   dispatch({
+          //     type: actionType.SET_USER,
+          //     user: data,
+          //   });
+          // });
+        });
+        // setIsLoading(false);
+      } else {
+        setAuth(false);
+        // dispatch({
+        //   type: actionType.SET_USER,
+        //   user: null,
+        // });
+        // setIsLoading(false);
+        window.localStorage.setItem("auth", "false");
+        navigate("/login");
+      }
+    });
+  }, []);
+
   return (
     <div>
-      App
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/*" element={<Home />} />
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import Header from "./Header";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+ return (
+    <div className="w-full h-auto flex flex-col items-center justify-center bg-primary">
+      <Header />
+      {/* <SearchBar />
+
+      {searchTerm.length > 0 && (
+        <p className="my-4 text-base text-textColor">
+          Searched for :
+          <span className="text-xl text-cartBg font-semibold">
+            {searchTerm}
+          </span>
+        </p>
+      )} */}
+
+      {/* <Filter setFilteredSongs={setFilteredSongs} /> */}
+
+      {/* <div className="w-full h-auto flex items-center justify-evenly gap-4 flex-wrap p-4">
+        <HomeSongContainer musics={filteredSongs ? filteredSongs : allSongs} />
+      </div> */}
+    </div>
+  );
+};
 
 export default Home
